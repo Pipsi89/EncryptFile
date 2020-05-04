cd61090dac8d8d25214d62df86d230d9e5ac89f0
Mingo89 <pirker.christian89@gmx.at>
2020-05-04 22:28:10 +0200

    Random generation

--- EncryptFile/Program.cs
+++ EncryptFile/Program.cs
@@ -19,9 +19,16 @@
       byte[] providerKey;
       byte[] initVector;
 
+      byte[] random32 = new Byte[32];
+      byte[] random16 = new Byte[16];
+
+      RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+      rng.GetBytes(random32);
+      rng.GetBytes(random16);
+      
       var aesAlg = new RijndaelManaged();
-      aesAlg.Key = new byte[32] { 118, 123, 23, 17, 161, 152, 35, 68, 126, 213, 16, 115, 68, 217, 58, 108, 56, 218, 5, 78, 28, 128, 113, 208, 61, 56, 10, 87, 187, 162, 233, 38 };
-      aesAlg.IV = new byte[16] { 33, 241, 14, 16, 103, 18, 14, 248, 4, 54, 18, 5, 60, 76, 16, 191 };
+      aesAlg.Key = random32;
+      aesAlg.IV = random16;
 
       providerKey = aesAlg.Key;
       initVector = aesAlg.IV;
