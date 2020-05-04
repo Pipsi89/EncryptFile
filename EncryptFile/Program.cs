d41670b274392f14d6edb49c4b21e27eb0338637
Mingo89 <pirker.christian89@gmx.at>
2020-05-04 22:17:08 +0200

    Eigener Key

--- EncryptFile/Program.cs
+++ EncryptFile/Program.cs
@@ -19,16 +19,21 @@
       byte[] providerKey;
       byte[] initVector;
 
+      var aesAlg = new RijndaelManaged();
+      aesAlg.Key = new byte[32] { 118, 123, 23, 17, 161, 152, 35, 68, 126, 213, 16, 115, 68, 217, 58, 108, 56, 218, 5, 78, 28, 128, 113, 208, 61, 56, 10, 87, 187, 162, 233, 38 };
+      aesAlg.IV = new byte[16] { 33, 241, 14, 16, 103, 18, 14, 248, 4, 54, 18, 5, 60, 76, 16, 191 };
+
+      providerKey = aesAlg.Key;
+      initVector = aesAlg.IV;
+
       //Encrypt and write to "testOutput"
       using (var sourceStream = File.OpenRead(sourceFilename))
       using (var destinationStream = File.Create(destinationFilename))
-      using (var provider = new AesCryptoServiceProvider())
-      using (var cryptoTransform = provider.CreateEncryptor())
+      using (var cryptoTransform = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV))
       using (var cryptoStream = new CryptoStream(destinationStream, cryptoTransform, CryptoStreamMode.Write))
       {
         sourceStream.CopyTo(cryptoStream);
-        providerKey = provider.Key;
-        initVector = provider.IV;
+
         Console.WriteLine(System.Convert.ToBase64String(providerKey));
         Console.WriteLine(System.Convert.ToBase64String(initVector));
       }
